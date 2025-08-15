91214d28aa1b0ad820d158508e9d3eb961c43955
Zeynep Dağtekin <zeynep.dagtekin@stu.fsm.edu.tr>
2025-08-15 10:34:41 +0300

    dokument deleted

--- apps/export.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import json
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
-from components.VideoSave.src.models.PackageModel import PackageModel as Package
-
-with open("data.json", "w") as f:
-    f.write(Package.schema_json(indent=2))
